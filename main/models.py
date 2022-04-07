1bc4b3a864746f42daeba86e1067f8bb9e889a6c
sylvaindnd <dndsylvain@gmail.com>
2022-04-07 10:45:12 +0200

    update

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
+from asyncio.unix_events import DefaultEventLoopPolicy
 from django.db import models
 
-# Create your models here.
+class Object(models.Model):
+    used = models.BooleanField(default=False)
+    image = models.CharField(max_length=512, default="", blank=True)
+    name = models.CharField(max_length=128, default="", blank=True)
+    description = models.CharField(max_length=256, default="", blank=True)
+    price = models.FloatField(default=0)
+    price_before = models.IntegerField(default=0)
+    price_after = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        self.price_before = int(str(self.price).split('.')[0])
+        self.price_after = int(str(self.price).split('.')[1])
+        super().save(*args, **kwargs)
--- main/views.py
+++ main/views.py
@@ -1,6 +1,28 @@
-from turtle import ht
+from pyexpat import model
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
+from .models import *
+import random
 
 def index(request):
-    return JsonResponse({'status':'ok'})
+    objects = [x for x in Object.objects.all() if not x.used]
+    
+    if(len(objects)==0):
+        for obj in Object.objects.all():
+            obj.used = False
+
+    obj = random.choice(objects)
+
+    result = {
+        'id': obj.id,
+        'image': obj.image,
+        'name': obj.name,
+        'description': obj.description,
+        'price': obj.price,
+        'price_before': obj.price_before,
+        'price_after': obj.price_after
+    }
+
+     
+    
+    return JsonResponse({'status': 'ok', 'object': result})
